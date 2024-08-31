652f34bb4fc832c40da8b0312d263fed10945cd8
Drivello <Nico_dd@outlook.com.ar>
2024-08-31 19:42:14 -0300

    task entity update and new taskbuider

--- src/core/entities/task.ts
+++ src/core/entities/task.ts
@@ -1,15 +1,41 @@
 import { Timestamp } from "@firebase/firestore";
 import { User } from "./user";
 
+export enum taskTypeEnum {
+    personal = "personal",
+    professional = "professional",
+    home = "home",
+}
+
+export enum taskStatus {
+    pending = "pending",
+    completed = "completed",
+}
+
+export enum taskRecurrency {
+    unique = "unique",
+    daily = "daily",
+    weekly = "weekly",
+    monthly = "monthly",
+}
+
 export interface Task {
     id: string;
     title: string;
     description?: string;
     dueDate?: Timestamp | undefined;
-    taskType: "personal" | "professional" | "home";
+    taskType:
+        | taskTypeEnum.personal
+        | taskTypeEnum.professional
+        | taskTypeEnum.home;
     assignedTo?: Omit<User, "personalTasks" | "professionalTasks" | "homeId">[];
     homeId?: string;
-    status: "pending" | "completed";
+    status: taskStatus.pending | taskStatus.completed;
     createdBy: Omit<User, "personalTasks" | "professionalTasks" | "homeId">;
-    recurrence?: "unique" | "daily" | "weekly" | "monthly";
+    recurrence?:
+        | taskRecurrency.unique
+        | taskRecurrency.daily
+        | taskRecurrency.weekly
+        | taskRecurrency.monthly;
 }
+
---
+++ src/core/entities/taskBuilder.ts
@@ -0,0 +1,93 @@
+import { Task, taskTypeEnum, taskStatus, taskRecurrency } from "./task";
+import { Timestamp } from "@firebase/firestore";
+import { User } from "./user";
+
+export class TaskBuilder {
+    private id: string = "";
+    private title: string = "";
+    private description?: string;
+    private dueDate?: Timestamp | undefined;
+    private taskType: taskTypeEnum = taskTypeEnum.personal;
+    private assignedTo: Omit<
+        User,
+        "personalTasks" | "professionalTasks" | "homeId"
+    >[] = [];
+    private homeId?: string;
+    private status: taskStatus = taskStatus.pending;
+    private createdBy: Omit<
+        User,
+        "personalTasks" | "professionalTasks" | "homeId"
+    >;
+    private recurrence: taskRecurrency = taskRecurrency.unique;
+
+    constructor(
+        createdBy: Omit<User, "personalTasks" | "professionalTasks" | "homeId">
+    ) {
+        this.createdBy = createdBy;
+    }
+
+    setId(id: string): TaskBuilder {
+        this.id = id;
+        return this;
+    }
+
+    setTitle(title: string): TaskBuilder {
+        this.title = title;
+        return this;
+    }
+
+    setDescription(description: string): TaskBuilder {
+        this.description = description;
+        return this;
+    }
+
+    setDueDate(dueDate: Timestamp | undefined): TaskBuilder {
+        this.dueDate = dueDate;
+        return this;
+    }
+
+    setTaskType(taskType: taskTypeEnum): TaskBuilder {
+        this.taskType = taskType;
+        return this;
+    }
+
+    setAssignedTo(
+        assignedTo: Omit<
+            User,
+            "personalTasks" | "professionalTasks" | "homeId"
+        >[]
+    ): TaskBuilder {
+        this.assignedTo = assignedTo;
+        return this;
+    }
+
+    setHomeId(homeId: string): TaskBuilder {
+        this.homeId = homeId;
+        return this;
+    }
+
+    setStatus(status: taskStatus): TaskBuilder {
+        this.status = status;
+        return this;
+    }
+
+    setRecurrence(recurrence: taskRecurrency): TaskBuilder {
+        this.recurrence = recurrence;
+        return this;
+    }
+
+    build(): Task {
+        return {
+            id: this.id,
+            title: this.title,
+            description: this.description,
+            dueDate: this.dueDate,
+            taskType: this.taskType,
+            assignedTo: this.assignedTo,
+            homeId: this.homeId,
+            status: this.status,
+            createdBy: this.createdBy,
+            recurrence: this.recurrence,
+        };
+    }
+}
