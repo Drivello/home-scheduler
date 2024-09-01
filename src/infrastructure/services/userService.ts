364706384148ec623b9073c5785e4221cb7796ca
Drivello <Nico_dd@outlook.com.ar>
2024-08-31 22:33:32 -0300

    user service new validations

--- src/infrastructure/services/userService.ts
+++ src/infrastructure/services/userService.ts
@@ -9,14 +9,17 @@ export class UserService {
     }
 
     async getUserById(id: string): Promise<User | null> {
+        if (!id) throw new Error("[UserService - GetUserById]: ID is required")
         return await this.userRepository.getById(id);
     }
 
     async getUserByEmail(email: string): Promise<User | null> {
+        if (!email) throw new Error("[UserService - GetUserByEmail]: Email is required")
         return await this.userRepository.getByEmail(email);
     }
 
     async getUsersFromEmails(emails: string[]): Promise<User[]> {
+        if (emails.length === 0 ) throw new Error("[UserService - GetUsersFromEmails]: Provided array is empty")
         const promises = emails.map((email) => this.getUserByEmail(email));
         const results = (await Promise.all(promises)) as User[];
         return results.filter((user): user is User => user !== null);
@@ -24,16 +27,30 @@ export class UserService {
 
     async getOrCreateUser(user: User): Promise<void> {
         const userByEmail = await this.getUserByEmail(user.email);
-
         if (!userByEmail) await this.userRepository.create(user);
     }
 
     async updateUser(user: User): Promise<User | null> {
+        if (!user || !user.id) throw new Error("[UserService - UpdateUser]: User ID is required")
         await this.userRepository.update(user);
         return await this.userRepository.getByEmail(user.email);
     }
 
     async deleteUser(id: string): Promise<void> {
         await this.userRepository.delete(id);
+    }
+
+    async deleteProfessionalTask(userId: string, taskId: string): Promise<void> {
+        let user = await this.userRepository.getById(userId);
+        if (user) user.professionalTasks = user?.professionalTasks.filter(task => task !== taskId)
+        else throw new Error("[UserService - DeleteProfessionalTask]: User not found")
+        await this.updateUser(user)
+    }
+
+    async deletePersonalTask(userId: string, taskId: string): Promise<void> {
+        let user = await this.userRepository.getById(userId);
+        if (user) user.personalTasks = user?.personalTasks.filter(task => task !== taskId)
+        else throw new Error("[UserService - DeletePersonalTask]: User not found")
+        await this.updateUser(user)
     }
 }
