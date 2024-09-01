d8684b7e9d8c4831b7ef55de3a891b8b57cbdfcf
Drivello <Nico_dd@outlook.com.ar>
2024-08-31 22:33:57 -0300

    taskservice duedate undefined fix

--- src/infrastructure/services/taskService.ts
+++ src/infrastructure/services/taskService.ts
@@ -31,6 +31,7 @@ export class TaskService {
 
     async updateTask(task: Task): Promise<void> {
         if (!task || task.id.length === 0) throw new Error("[Task Service - UpdateTask]: Cannot update task without task / task id"); 
+        if (task.dueDate === undefined) delete task.dueDate;
         await this.taskRepository.update(task);
     }
 
